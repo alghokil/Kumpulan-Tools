6a181cc006f3d2bb67a91dc4712fa6c1812a81c1
alghokil <54709070+alghokil@users.noreply.github.com>
2019-10-07 23:50:20 +0700

    Add files via upload

---
+++ Kumpulan-Tools.sh
@@ -0,0 +1,557 @@
+#!/bin/bash
+clear
+echo " ____________________________________"
+echo "| Kami berjanji sesuai dengan harapan|"
+echo "|   Kami dan melakukan sesuai dengan |"
+echo "|           Katakutan kami           |"
+echo "|____________________________________|"
+read -p "Password : " pass
+if [ $pass = "Kltg" ]
+then
+clear
+toilet -f slant --gay "Gunakanilmukaliandenganbijak"
+sleep 1
+clear
+else
+  echo "\033[31;1mIncorrect"
+  echo "\033[37;1m"
+  sleep 300
+  sh 991.sh
+fi
+clear
+echo " ##    ## ##     ## ##     ## ########  ##     ## ##          ###    ##    ## ########  #######   #######  ##        ######" | lolcat
+echo " ##   ##  ##     ## ###   ### ##     ## ##     ## ##         ## ##   ###   ##    ##    ##     ## ##     ## ##       ##    ##" | lolcat
+echo " ##  ##   ##     ## #### #### ##     ## ##     ## ##        ##   ##  ####  ##    ##    ##     ## ##     ## ##       ##" | lolcat
+echo " #####    ##     ## ## ### ## ########  ##     ## ##       ##     ## ## ## ##    ##    ##     ## ##     ## ##        ######" | lolcat
+echo " ##  ##   ##     ## ##     ## ##        ##     ## ##       ######### ##  ####    ##    ##     ## ##     ## ##             ##" | lolcat
+echo " ##   ##  ##     ## ##     ## ##        ##     ## ##       ##     ## ##   ###    ##    ##     ## ##     ## ##       ##    #" | lolcat
+echo " ##    ##  #######  ##     ## ##         #######  ######## ##     ## ##    ##    ##     #######   #######  ########  ######" | lolcat
+
+echo "════════════════════════════════════════════════════════════════════════════════════" | lolcat
+echo " # Creadby   : Al Ghokil" | lolcat
+echo " # Team      : Cyber Muslim Uknnown Friend" | lolcat
+echo " # Contact   : 0812xxxxxxxxxxxxxxx" | lolcat
+echo "════════════════════════════════════════════════════════════════════════════════════" | lolcat
+echo""
+
+echo "1.BAJINGANv6" 
+echo "2.Rusmana-ID"
+echo "3.Readskull"
+echo "4.ZK1-CYBER"
+echo "5.TOOLSINSTALLERv1"
+echo "6.blackeye"
+echo "7.Project-X666X"
+echo "8.MIXINGS"
+echo "9.ACN"
+echo "10.ToolsLengkap"
+echo "11.ACNv2"
+echo "12.Tool-X"
+echo "13.STARK2.0"
+echo "14.AK47"
+echo "15.DarkFly-Tool"
+echo "16.EasY_HaCk"
+echo "17.bomber"
+echo "18.HiddenEye"
+echo "19.Amer"
+echo "20.mrcakil"
+echo "21.InstallBucin"
+echo "22.saycheese"
+echo "23.DDOS"
+echo "24.wifi-hacker+root"
+echo "25.dicky"
+echo "26.KALSEL-Z-TOOL"
+echo "27.KANGHACK"
+echo "28.TOOLSKEREN "
+echo "29.NewsX"
+echo "30.santri"
+echo "31.BYE"
+echo "32.YT-tools"
+echo "33.mrcakil"
+echo "34.shellphish"
+echo "35.THT"
+echo "36.Cl0neMast3r"
+echo "37.RDizV2"
+echo "38.santet-online"
+echo "39.SETAN"
+echo "40.VirusMaker404"
+read -p "root@T00Ls-alghokil~#" bro
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv4
+mv B4J1N64Nv4 $HOME
+cd $HOME/B4J1N64Nv4
+sh bajingan.sh
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/Rusmana-ID/rus
+mv rus $HOME
+cd $HOME/rus
+sh v2.sh
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/C-H-T/Readskull
+mv Readskull $HOME
+cd $HOME/Readskull
+sh ReadSkull.sh
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/ZK1Hacker/ZK1-CYBER
+mv ZK1-CYBER $HOME
+cd $HOME/ZK1-CYBER
+sh ZK1.sh
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+mv TOOLSINSTALLERv1 $HOME
+cd $HOME/TOOLSINSTALLERv1
+./Tuanb4dut.sh
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/thelinuxchoice/blackeye
+mv blackeye $HOME
+cd $HOME/blackeye
+sh blackeye.sh
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/Fukuro-ExP/Project-X666X
+mv Project-X666X $HOME
+cd $HOME/Project-X666X
+python2 X666X.py
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/mbest99/MIXINGS
+mv MIXINGS $HOME
+cd $HOME/MIXINGS
+sh Oppay.sh
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/MaulanaRyM/ACN
+mv ACN $HOME
+cd $HOME/ACN
+sh ACN.sh
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/rahmadxyz/ToolsLengkap
+mv ToolsLengkap $HOME
+cd $HOME/ToolsLengkap
+sh rahmad.sh
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/MaulanaRyM/ACNv2
+mv ACNv2 $HOME
+cd $HOME/ACNv2
+sh ACNv2.sh
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/Rajkumrdusad/Tool-X
+mv Tool-X $HOME
+cd $HOME/Tool-X
+./install.aex
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/aniketstark/STARK2.0
+mv STARK2.0 $HOME
+cd $HOME/STARK2.0
+python2 stark.py
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/nasirxo/AK47
+mv AK47 $HOME
+cd $HOME/AK47
+python2 ak47.py
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/Ranginang67/DarkFly-Tool
+mv DarkFly-Tool $HOME
+cd $HOME/DarkFly-Tool
+python2 install.py
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/sabri-zaki/EasY_HaCk
+mv EasY_HaCk $HOME
+cd $HOME/EasY_HaCk
+bash install.sh
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/Syhrularv/bomber
+mv bomber $HOME
+cd $HOME/bomber
+bash bom.sh
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+https://github.com/DarkSecDevelopers/HiddenEye
+mv HiddenEye $HOME
+cd $HOME/HiddenEye
+python3 HiddenEye.py
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+https://github.com/Amerlaceset/Amer
+mv Amer $HOME
+cd $HOME/Amer
+./Amer.sh
+fi
+
+if [ $bro = 20 ] || [ $bro = 20 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/Mrcakil/mrcakil
+mv mrcakil $HOME
+cd $HOME/mrcakil
+./tools.sh
+fi
+
+if [ $bro = 21 ] || [ $bro = 21 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/MrGameOver16/InstallBucin
+mv InstallBucin $HOME
+cd $HOME/InstallBucin
+bash install.sh
+sh bucin.sh
+fi
+
+if [ $bro = 22 ] || [ $bro = 22 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/thelinuxchoice/saycheese
+mv saycheese $HOME
+cd $HOME/saycheese
+sh saycheese.sh
+fi
+
+if [ $bro = 23 ] || [ $bro = 23 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/alghokil/DDOS
+mv DDOS $HOME
+cd $HOME/DDOS
+bash Toolsx.sh
+fi
+
+if [ $bro = 24 ] || [ $bro = 24 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/esc0rtd3w/wifi-hacker
+mv wifi-hacker $HOME
+cd $HOME/wifi-hacker
+sh wifi-hacker.sh
+fi
+
+if [ $bro = 25 ] || [ $bro = 25 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/DICKYalone/dicky
+mv dicky $HOME
+cd $HOME/dicky
+sh Dickygt.sh
+fi
+
+if [ $bro = 26 ] || [ $bro = 26 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/KALSELeXploit/KALSEL-Z-TOOL
+mv KALSEL-Z-TOOL $HOME
+cd $HOME/KALSEL-Z-TOOL
+sh Z-TOOL.sh
+fi
+
+if [ $bro = 27 ] || [ $bro = 27 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/KANG-BADRU/KANGHACK
+mv KANGHACK $HOME
+cd $HOME/KANGHACK
+python2 KANGHACKDARK.py
+fi
+
+if [ $bro = 28 ] || [ $bro = 28 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/LuciferXtools/TOOLSKEREN
+mv TOOLSKEREN $HOME
+cd $HOME/TOOLSKEREN
+sh FAKE.sh
+fi
+
+if [ $bro = 29 ] || [ $bro = 29 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/pengangguranmuda/NewsX
+mv NewsX $HOME
+cd $HOME/NewsX
+./newsx.sh
+fi
+
+if [ $bro = 30 ] || [ $bro = 30 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/yunusrespector/santri
+mv santri $HOME
+cd $HOME/santri
+sh Gwganssangat
+fi
+
+if [ $bro = 31 ] || [ $bro = 31 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/wahyuandhika/BYE
+mv BYE $HOME
+cd $HOME/BYE
+python2 BYE.py
+fi
+
+if [ $bro = 32 ] || [ $bro = 32 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone git clone https://github.com/kucing-hitam/YT-tools
+mv YT-tools $HOME
+cd $HOME/YT-tools
+sh YT-tools.sh
+fi
+
+if [ $bro = 33 ] || [ $bro = 33 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/Mrcakil/mrcakil
+mv mrcakil $HOME
+cd $HOME/mrcakil
+./tools2
+fi
+
+if [ $bro = 34 ] || [ $bro = 34 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/thelinuxchoice/shellphish
+mv shellphish $HOME
+cd $HOME/shellphish
+bash shellphish.sh
+fi
+
+if [ $bro = 35 ] || [ $bro = 35 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/Mrv4n/THT
+mv THT $HOME
+cd $HOME/THT
+sh THT.sh
+fi
+
+if [ $bro = 36 ] || [ $bro = 36 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/Abdulrah33m/Cl0neMast3r
+mv Cl0neMast3r $HOME
+cd $HOME/Cl0neMast3r
+python2 Cl0neMast3r.py
+fi
+
+if [ $bro = 37 ] || [ $bro = 37 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/dizline/RDizV2
+mv RDizV2 $HOME
+cd $HOME/RDizV2
+sh RDizV2.sh
+fi
+
+if [ $bro = 38 ] || [ $bro = 38 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/Gameye98/santet-online
+mv santet-online $HOME
+cd $HOME/santet-online
+python santet.py
+fi
+
+if [ $bro = 39 ] || [ $bro = 39 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/pashayogi/SETAN
+mv SETAN $HOME
+cd $HOEM/SETAN
+python2 SETAN.py
+fi
+
+if [ $bro = 40 ] || [ $bro = 40 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/Funkyz86/VirusMaker404
+mv VirusMaker404 $Home
+cd $HOME/VirusMaker404
+python VirusMaker.py
+fi
